f11208c8067e60665a3594fc61d5d09fe52ad967
ItsSpirax <60130987+ItsSpirax@users.noreply.github.com>
2024-05-24 04:06:21 +0530

    Initialize Dockerized Express Application with Reverse Proxy

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+require('dotenv').config();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'API Gateway',
+    text: process.env.DISPLAY_TEXT
+  });
+});
+
+module.exports = router;
