bc47beb9d5fe1e638e8f91cd6870a3d3ad9f581a
Manan Gandhi <ardumanan@gmail.com>
2024-05-24 09:10:29 +0530

    Tested custom headers (authorization)

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var proxy = require('express-http-proxy');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var proxy = require("express-http-proxy");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/proxy', proxy(process.env.PROXY_URL));
+app.use("/", indexRouter);
+app.use(
+  "/proxy",
+  proxy(process.env.PROXY_URL, {
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      const token = "";
+      console.log(proxyReqOpts);
+      proxyReqOpts.headers = {
+        ...proxyReqOpts,
+        authorization: `Bearer ${token}`,
+      };
+      return proxyReqOpts
+    },
+  })
+);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { 
-    title: 'Error'
+  res.render("error", {
+    title: "Error",
   });
 });
 
