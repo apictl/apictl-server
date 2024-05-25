03dc32e74c61c3b4f044e6b37a5b37bd0a31efed
ItsSpirax <60130987+ItsSpirax@users.noreply.github.com>
2024-05-25 22:31:19 +0530

    Fix SSL

--- gateway-server/app.js
+++ gateway-server/app.js
@@ -1,7 +1,5 @@
 var createError = require("http-errors");
 var express = require("express");
-var https = require("https");
-var fs = require("fs");
 var proxy = require("express-http-proxy");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -12,16 +10,9 @@ const authRouter = require("./routes/auth");
 const projectRouter = require("./routes/project");
 const projectConfigRouter = require("./routes/project_config");
 
-require("dotenv").config();
-
-const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/${process.env.DOMAIN}/privkey.pem"), 
-  cert: fs.readFileSync("/etc/letsencrypt/live/${process.env.DOMAIN}/fullchain.pem")
-};
-
 var app = express();
 
-var server = https.createServer(options, app);
+require("dotenv").config();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -79,6 +70,3 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
-
-server.listen(3000);
-console.log('HTTPS Server running on port 3000');
