204d5440c2929c6f30c4f7defbaf8598a8817111
Manan Gandhi <ardumanan@gmail.com>
2024-05-27 00:34:13 +0530

    Feat: Added cors

--- gateway-server/routes/proxy.js
+++ gateway-server/routes/proxy.js
@@ -5,22 +5,22 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 
 router.use(
-    "/",
-    createProxyMiddleware({
-      target: process.env.PROXY_URL,
-      changeOrigin: true,
-      on: {
-        proxyReq: (proxyReq, req, res) => {
-          proxyReq.setHeader(
-            "Authorization",
-            `Bearer ${process.env.TEST_API_KEY}`
-          );
-        },
-        proxyRes: (proxyRes, req, res) => {
-          res.statusCode = proxyRes.statusCode;
-        },
+  "/",
+  createProxyMiddleware({
+    target: process.env.PROXY_URL,
+    changeOrigin: true,
+    on: {
+      proxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader(
+          "Authorization",
+          `Bearer ${process.env.TEST_API_KEY}`
+        );
       },
-    })
-  );
+      proxyRes: (proxyRes, req, res) => {
+        res.statusCode = proxyRes.statusCode;
+      },
+    },
+  })
+);
 
 module.exports = router;
