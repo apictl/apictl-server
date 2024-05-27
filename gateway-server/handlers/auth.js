56525fb05544b9e0b3a9452979747733b403dfa8
ItsSpirax <60130987+ItsSpirax@users.noreply.github.com>
2024-05-27 08:39:18 +0530

    Cleanup env and Code

--- gateway-server/handlers/auth.js
+++ gateway-server/handlers/auth.js
@@ -57,7 +57,12 @@ const registerHandler = async (req, res) => {
   });
   user.password = undefined;
   const token = jwt.sign({ name, email, id: user.id }, jwtSecret);
-  const url = `${domain}auth/verify?token=${token}`;
+  
+  if (domain.endsWith("/")) {
+    domain = domain.slice(0, -1);
+  }
+  
+  const url = `${domain}/auth/verify?token=${token}`;
   await sendEmail(
     email,
     "Verify",
