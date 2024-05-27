d5d73b995748e0537ce1f91309050cc39f8da846
ItsSpirax <60130987+ItsSpirax@users.noreply.github.com>
2024-05-27 08:18:13 +0530

    Setup Proxying and Geo Restriction

--- gateway-server/app.js
+++ gateway-server/app.js
@@ -29,10 +29,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/proxy", proxyRouter);
 app.use("/auth", authRouter);
 app.use("/project", projectRouter);
 app.use("/project/:id", projectConfigRouter);
+app.use("/:project/:endpoint/*", proxyRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ gateway-server/handlers/proxy.js
@@ -0,0 +1,85 @@
+const { PrismaClient } = require("@prisma/client");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const getCountry = require("../utils/geolite");
+const prisma = new PrismaClient()
+
+require("dotenv").config();
+
+const proxyHandler = async (req, res) => {
+    const { project, endpoint } = req.params;
+    const country = await getCountry(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+    const projectRecord = await prisma.project.findUnique({
+        where: {
+            id: project,
+        },
+    });
+
+    const endpointRecord = await prisma.endpoint.findUnique({
+        where: {
+            id: endpoint,
+        },
+    });
+
+    if (!projectRecord || !endpointRecord || projectRecord.id !== endpointRecord.projectId) {
+        return res.status(404).json({
+            success: false,
+            message: "Not Found",
+            data: null,
+        });
+    }
+
+    const blackListedCountries = endpointRecord.blackListedCountries || [];
+    if (blackListedCountries.includes(country)) {
+        return res.status(403).json({
+            success: false,
+            message: `Forbidden ${country}`,
+            data: null,
+        });
+    }
+
+    const allowedOrigins = endpointRecord.allowedOrigins || [];
+    if (allowedOrigins.length > 0 && !allowedOrigins.includes(req.headers.origin)) {
+        return res.status(403).json({
+            success: false,
+            message: "Forbidden",
+            data: null,
+        });
+    }
+
+    const allowedShaKeys = endpointRecord.allowedShaKeys || [];
+    if (allowedShaKeys.length > 0 && !allowedShaKeys.includes(req.headers['x-sha-key'])) {
+        return res.status(403).json({
+            success: false,
+            message: "Forbidden",
+            data: null,
+        });
+    }
+
+    if (endpointRecord.url.endsWith("/")) {
+        endpointRecord.url = endpointRecord.url.slice(0, -1);
+    }
+
+    const url = endpointRecord.url + req.originalUrl.replace(`/${project}/${endpoint}`, "");
+
+    const proxy = createProxyMiddleware({
+        target: url,
+        changeOrigin: true,
+        on: {
+          proxyReq: (proxyReq, req, res) => {
+            proxyReq.setHeader(
+              "Authorization",
+              `Bearer ${endpointRecord.apiKey}`
+            );
+          },
+          proxyRes: (proxyRes, req, res) => {
+            res.statusCode = proxyRes.statusCode;
+          },
+        },
+      })
+
+    proxy(req, res);
+}
+
+module.exports = {
+    proxyHandler,
+};
