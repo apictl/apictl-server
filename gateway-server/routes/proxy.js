43aefce9177dacff3b813ad883ddb614abeeab5b
Manan Gandhi <ardumanan@gmail.com>
2024-05-30 15:00:27 +0530

    Made proxy rate limiter base file

--- gateway-server/routes/proxy.js
+++ gateway-server/routes/proxy.js
@@ -3,7 +3,8 @@ var router = express.Router({ mergeParams: true });
 
 const { proxyHandler } = require("../handlers/proxy");
 const { proxyVerification } = require("../middlewares/proxy_verification");
+const { proxyRateLimiter } = require("../middlewares/proxy_rate_limiter");
 
-router.use("/", proxyVerification, proxyHandler);
+router.use("/", proxyVerification, proxyRateLimiter, proxyHandler);
 
 module.exports = router;
