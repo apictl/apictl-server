45a035eff10c7e0fa2ce88192a9865dda04c375e
Manan Gandhi <ardumanan@gmail.com>
2024-05-31 18:16:35 +0530

    Feat: Added fields for proxy analytics

--- gateway-server/middlewares/proxy_analytics.js
+++ gateway-server/middlewares/proxy_analytics.js
@@ -1,5 +1,6 @@
 const proxyAnalytics = (req, res, next) => {
   const start = process.hrtime();
+  const reqTime = Date.now();
   res.on("finish", () => {
     const end = process.hrtime();
     const ms = (end[0] - start[0]) * 1e3 + (end[1] - start[1]) * 1e-6;
@@ -8,9 +9,16 @@ const proxyAnalytics = (req, res, next) => {
       origin: req.get("origin"),
       project: req.params.project,
       endpoint: req.params.endpoint,
-      time: Date.now(),
+      time: reqTime,
       path: "/" + req.baseUrl.split("/").slice(3).join("/"),
+      userAgent: req.headers["user-agent"] || "",
+      ip:
+        req.headers["x-forwarded-for"] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress,
     };
+    console.log(data);
   });
   next();
 };
