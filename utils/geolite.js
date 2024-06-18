98df66e0ab0d24df16c499226d0aa95f32148c21
Manan Gandhi <ardumanan@gmail.com>
2024-06-18 23:47:07 +0530

    Refactor: formatted all code

--- utils/geolite.js
+++ utils/geolite.js
@@ -1,8 +1,8 @@
-const Reader = require('@maxmind/geoip2-node').Reader;
+const Reader = require("@maxmind/geoip2-node").Reader;
 
 const getCountry = async (ip) => {
   try {
-    const reader = await Reader.open('./utils/GeoLite2/GeoLite2-Country.mmdb');
+    const reader = await Reader.open("./utils/GeoLite2/GeoLite2-Country.mmdb");
     const response = reader.country(ip);
     return response.country.isoCode;
   } catch (error) {
